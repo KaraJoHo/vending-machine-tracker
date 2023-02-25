20532a9c5bc176a7165fee4489681f1567a7979d
KaraJH <karas.jones@yahoo.com>
2023-02-24 17:15:04 -0700

    feat: Create join table for snacks and machines. Passes model tests

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,7 @@
 class Machine < ApplicationRecord
-  validates_presence_of :location
+  validates_presence_of :location, :name
   
   belongs_to :owner
+  has_many :machine_snacks 
+  has_many :snacks, through: :machine_snacks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914040805) do
+ActiveRecord::Schema.define(version: 2023_02_25_001345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "machine_snacks", force: :cascade do |t|
+    t.bigint "machine_id"
+    t.bigint "snack_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["machine_id"], name: "index_machine_snacks_on_machine_id"
+    t.index ["snack_id"], name: "index_machine_snacks_on_snack_id"
+  end
+
   create_table "machines", force: :cascade do |t|
     t.string "location"
     t.bigint "owner_id"
+    t.string "name"
     t.index ["owner_id"], name: "index_machines_on_owner_id"
   end
 
@@ -25,5 +35,14 @@ ActiveRecord::Schema.define(version: 20170914040805) do
     t.string "name"
   end
 
+  create_table "snacks", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "machine_snacks", "machines"
+  add_foreign_key "machine_snacks", "snacks"
   add_foreign_key "machines", "owners"
 end
