7da0c05d5034c4efb7433239420b73781c3c5721
KaraJH <karas.jones@yahoo.com>
2023-02-24 18:34:47 -0700

    test: Average price in model and view

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
     snack_in_machine_1 = MachineSnack.create!(snack_id: snack_2.id, machine_id: dons.id)
     
     visit machine_path(dons.id)
-    # require 'pry'; binding.pry
 
     within("#snack_#{snack_1.id}") do 
       expect(page).to have_content("Chips")
@@ -36,6 +35,26 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
       expect(page).to have_content(snack_2.name)
       expect(page).to_not have_content(snack_3.name)
 
+    end
+  end
+
+  scenario 'has the average price for all snacks' do 
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    snack_1 = Snack.create!(name: "Chips", price: 1)
+    snack_2 = Snack.create!(name: "Chocolate", price: 2)
+    snack_3 = Snack.create!(name: "Berries", price: 3)
+
+    snack_in_machine_1 = MachineSnack.create!(snack_id: snack_1.id, machine_id: dons.id)
+    snack_in_machine_1 = MachineSnack.create!(snack_id: snack_2.id, machine_id: dons.id)
+    
+    visit machine_path(dons.id)
+    # require 'pry'; binding.pry
+
+
+    within(".snacks") do 
+      expect(page).to have_content("Average Price: $1.50")
     end
   end
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe Machine, type: :model do
     it { should have_many :machine_snacks}
     it { should have_many :snacks}
   end
+
+  describe "#average_price_of_snacks" do 
+    it 'is the average price of snacks in the machine' do 
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    snack_1 = Snack.create!(name: "Chips", price: 1)
+    snack_2 = Snack.create!(name: "Chocolate", price: 2)
+    snack_3 = Snack.create!(name: "Berries", price: 3)
+
+    snack_in_machine_1 = MachineSnack.create!(snack_id: snack_1.id, machine_id: dons.id)
+    snack_in_machine_1 = MachineSnack.create!(snack_id: snack_2.id, machine_id: dons.id)
+    
+    expect(dons.average_price_of_snacks.to_f).to eq(1.5)
+    end
+  end
 end
