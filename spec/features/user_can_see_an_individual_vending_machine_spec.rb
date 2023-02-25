2773f1bb45dc3dd0a5dbfcecb83f2011772d3e11
KaraJH <karas.jones@yahoo.com>
2023-02-24 17:46:47 -0700

    test: User story 1, snacks on machine show page

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -5,8 +5,37 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
 
-    visit machine_path(dons)
+    visit machine_path(dons.id)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+  end
+
+  scenario 'it has the name of all snacks associated with that machine' do 
+
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    snack_1 = Snack.create!(name: "Chips", price: 1)
+    snack_2 = Snack.create!(name: "Chocolate", price: 2)
+    snack_3 = Snack.create!(name: "Berries", price: 3)
+
+    snack_in_machine_1 = MachineSnack.create!(snack_id: snack_1.id, machine_id: dons.id)
+    snack_in_machine_1 = MachineSnack.create!(snack_id: snack_2.id, machine_id: dons.id)
+    
+    visit machine_path(dons.id)
+    # require 'pry'; binding.pry
+
+    within("#snack_#{snack_1.id}") do 
+      expect(page).to have_content("Chips")
+      expect(page).to have_content("Price: 1")
+      expect(page).to_not have_content("Chocolate")
+    end
+
+    within(".snacks") do 
+      expect(page).to have_content(snack_1.name)
+      expect(page).to have_content(snack_2.name)
+      expect(page).to_not have_content(snack_3.name)
+
+    end
   end
 end
